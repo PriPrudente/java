ae53146686e7615e7ef1867811c56293abe70205
PriPrudente <priscila.prudente.fernandes@gmail.com>
2022-02-18 16:24:29 -0300

    Aula Java

---
+++ src/br/uninove/poo/Pessoa.java
@@ -0,0 +1,10 @@
+package br.uninove.poo;
+
+public class Pessoa {
+    String nome; //string significa texto
+    String cpf;
+    int idade; //int = numeros inteiros
+    char sexo; //char = apenas UM caractere
+    float peso; //valor real 
+    boolean fumante; //boolean = verdadeiro ou falso 
+}
